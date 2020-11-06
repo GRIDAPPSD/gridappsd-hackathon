376c2cfec98be3378d571704cca0e0f7630cb333
Alexander Anderson <alexander.a.anderson@ieee.org>
2020-11-06 00:03:03 -0800

    11/5 cloud backup

--- Run123NodeSensorServiceDemo.py
+++ Run123NodeSensorServiceDemo.py
@@ -3,9 +3,6 @@ from gridappsd import GridAPPSD
 from gridappsd.simulation import Simulation
 
 
-models = dict(ieee123=u'_C1C3E687-6FFD-C753-582B-632A27E28507',
-              ieee8500=u'_4F76A5F9-271D-9EB8-5E31-AA362D86F2C3',
-              ieee13nodeckt=u'_49AD8E07-3BF9-A4E2-CB8F-C3722F837B62')
 run_config_123 = {
     "power_system_config": {
         "GeographicalRegion_name": "_73C512BD-7249-4F50-50DA-D93849B89C43",
